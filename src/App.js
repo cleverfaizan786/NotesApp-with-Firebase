bebaf4257edd02d40536890bf622db9d687b3a7f
faizan <faizanzaid789@gmail.com>
2022-01-30 11:38:18 +0530

    Added navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Header from './compoents/Header';
 
 export default class App extends Component {
   render() {
 	return <div>
+<Header/>
 
-		
 	</div>;
   }
 }
